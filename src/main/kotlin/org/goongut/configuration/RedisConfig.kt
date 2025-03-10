e13669fcd4f507b07a57f146ca441ec2e113827b
Ewan Jee <830jiforcareer@gmail.com>
2025-03-10 23:17:11 +0900

    feat: database setting

--- src/main/kotlin/org/goongut/configuration/RedisConfig.kt
+++ src/main/kotlin/org/goongut/configuration/RedisConfig.kt
@@ -1,4 +1,34 @@
 package org.goongut.configuration
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@EnableRedisRepositories
+@Configuration
 class RedisConfig {
+    @Value("\${spring.data.redis.host}")
+    lateinit var host: String
+
+    @Value("\${spring.data.redis.port}")
+    lateinit var port: String
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory = LettuceConnectionFactory(host, port.toInt())
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<*, *> =
+        RedisTemplate<Any, Any>().apply {
+            this.connectionFactory = redisConnectionFactory()
+
+            // // "\xac\xed\x00" 같은 불필요한 해시값을 보지 않기 위해 serializer 설정
+            this.keySerializer = StringRedisSerializer()
+            this.hashKeySerializer = StringRedisSerializer()
+            this.valueSerializer = StringRedisSerializer()
+        }
 }
